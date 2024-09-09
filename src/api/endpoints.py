f5517aad8ad5d8974fb399f61f26c6c8c07060d2
Radeep Yenni <radeeplucky143@gmail.com>
2024-09-09 22:25:50 +0530

    logger statements added successfully.

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, HTTPException
-
 from src.utils.yaml_util import Yaml
 from src.utils.subprocess_utils import SubProcess
+from src.utils.logger_util import Logger
 import asyncio
 
 router = APIRouter()
 
-@router.post("/create-node")
+@router.post("/create_node")
 async def create_node(node_name: str):
     """
     Creates a new Tailscale node with the given name.
@@ -17,17 +17,19 @@ async def create_node(node_name: str):
     """
 
     try:
+        logger = Logger(__name__).create_log_file()
         commands = Yaml().load_yaml()
-        SubProcess.execute(commands['node_creation'], success_code=201, message='Node creation failed')
-        return {"status": "Node created and authenticated successfully"}
 
-    except HTTPException as e:
-        raise e
+        SubProcess().execute(commands['node_creation'], success_code=201, message='Tailscale Node creation failed')
+        logger.info('Tailscale node created successfully.')
+        return {"message": "Tailscale Node created and authenticated successfully", "status_code": 201}
+
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Internal server error: {e}")
+        logger.error(f'Exception: {e}')
+        return {"status_code":500, "message":f"{e}")
 
 
-@router.post("/start-ray-cluster")
+@router.post("/start_ray_cluster")
 async def start_ray_cluster():
     """
     Starts a Ray cluster using Docker containers.
@@ -38,15 +40,20 @@ async def start_ray_cluster():
     """
 
     try:
+        logger = Logger(__name__).create_log_file()
         commands = Yaml().load_yaml()
 
-        SubProcess.execute(commands['build_ray_head'], success_code=201, message='Building Ray head image failed')
-        SubProcess.execute(commands['running_ray_head'], success_code=201, message='Running Ray head container failed')
-        SubProcess.execute(commands['build_ray_worker'], success_code=201, message='Building Ray worker image failed')
-        SubProcess.execute(commands['run_ray_worker'], success_code=201, message='Running Ray worker container failed')
-        return {"status": "Ray cluster started successfully"}
+        SubProcess().execute(commands['build_ray_head'], success_code=201, message='Building Ray head image failed')
+        logger.info('Building Ray head image successfully.')
+        SubProcess().execute(commands['running_ray_head'], success_code=201, message='Running Ray head container failed')
+        logger.info('Running Ray head container successfully.')
+        SubProcess().execute(commands['build_ray_worker'], success_code=201, message='Building Ray worker image failed')
+        logger.info('Building Ray worker image successfully')
+        SubProcess().execute(commands['run_ray_worker'], success_code=201, message='Running Ray worker container failed')
+        logger.info('Running Ray worker container successfully')
+        return {"status_code": 200, "message": "Ray cluster started successfully"}
 
-    except HTTPException as e:
-        raise e
+
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Internal server error: {e}")
+        logger.error(f'Exception: {e}')
+        return {"status_code":500, "message":f"{e}")
