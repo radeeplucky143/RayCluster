fa3ae1c210f3a3618d63e05b887d9c9c8d24f92b
Radeep Yenni <radeeplucky143@gmail.com>
2024-09-09 22:31:36 +0530

    logger statements added for every parameter for process execution.

--- src/utils/subprocess_utils.py
+++ src/utils/subprocess_utils.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 import asyncio
 
 
@@ -9,8 +10,10 @@ class SubProcess:
         self.stderr = None
         self.status_code = None
         self.message = None
+        self.logger = Logger(__name__).create_log_file()
 
     async def execute(self, command, success_code, message):
+        self.logger.info(f'command: {' '.join(command)} executing.')
         self.process = await asyncio.create_subprocess_exec(
             *command,
             stdout=asyncio.subprocess.PIPE,
@@ -20,7 +23,11 @@ class SubProcess:
         self.stdout, self.stderr = await self.process.communicate()
 
         if self.process.returncode != 0:
+            self.logger.error(f'Error: {self.stderr}')
+            self.logger.error(f'status_code: {success_code}')
             raise HTTPException(status_code=500, detail=f"{message}: {stderr.decode()}")
 
+        self.logger.info(f'Output: {self.stdout}')
+        self.logger.info(f'status_code: {status_code}')
         self.status_code = success_code
         self.message = message
