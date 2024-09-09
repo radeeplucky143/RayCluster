f973f8084b677ada921e4e06d540b93acd7d815e
Radeep Yenni <radeeplucky143@gmail.com>
2024-09-09 21:59:41 +0530

    logger statements added

--- src/utils/yaml_util.py
+++ src/utils/yaml_util.py
@@ -1,23 +1,19 @@
 import yaml
+from logger_util import Logger
 
 class Yaml:
 
     def __init__(self):
         self.file_name = 'commands.yaml'
         self.data = None
+        self.logger = Logger(__name__).create_log_file()
 
     def load_yaml(self):
-        """
-        Loads a YAML file and returns a python object.
-
-        Returns:
-            Python Dictionary with all the commands as values.
-        """
-
         try:
             with open(self.file_name, 'r') as f:
+                self.logger.info(f'Loading the {self.file_name}')
                 self.data = yaml.safe_load(f)
                 return self.data
 
         except yaml.YAMLError as e:
-            print(f"Error parsing YAML file: {e}")
+            self.logger.error(f"Error parsing YAML file: {e}")
